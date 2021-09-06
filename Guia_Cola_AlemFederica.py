66618210f04b4651cd6adeafd26cc50c7259ecfb
Federica Alem <alemfederica00@outlook.com>
2021-09-06 20:19:13 -0300

    Ejercicio 6 Resuelto

--- Guia_Cola_AlemFederica.py
+++ Guia_Cola_AlemFederica.py
@@ -38,6 +38,8 @@ print(p.sacar()) # imprime el 4 y lo saca
 print(p.sacar()) # impreme perro y lo saca
 print(p.tamanio()) # 2
 """
+
+
 print()
 
 # Ejercicio 4: 
@@ -129,3 +131,105 @@ c1.agregar("Romina")
 trasladar(c1)
 
 assert c1.es_vacia() == True
+
+
+print()
+# Ejercicio 6
+""" Escriba un metodo de Cola que dada una cola C1 reciba una cola C2  de 
+números enteros y devuelva una nueva Cola con los elementos concatenados en el 
+orden C1 y C2. Es de destacar que las Colas recibidas no deben sufrir ningún 
+tipo de cambio o alteración.(en principio utilizar los métodos de cola para la 
+tarea)"""
+
+class Cola:
+    
+    def __init__(self):
+      self.elementos = []
+      
+      
+    def agregar(self, item):
+        self.elementos.append(item)
+
+
+    def es_vacia(self)->bool: 
+        return self.elementos == []
+
+
+    def primero(self):
+        if not self.es_vacia():
+            return self.elementos[0]
+
+
+    def tamanio(self):
+        return len(self.elementos)
+
+
+    def sacar(self):
+        if not self.es_vacia():
+            return self.elementos.pop(0)
+        
+        
+    def __str__(self):
+        return ("{}".format(self.elementos))
+
+
+    def imprimir_Cola(self):
+       
+        for i in self.elementos:
+                 print(i)
+
+
+    def vaciar_Cola(self):       
+        while self.es_vacia() != []:
+            self.elementos.pop(0)
+            if self.elementos == []:
+                return True            
+
+
+    def dar_vuelta(self):
+        nuevaCola =[]
+        
+        while not self.es_vacia():                
+            nuevaCola.append(self.sacar())
+        
+        for i in nuevaCola:
+             self.elementos.insert(0, i)
+             
+             
+             
+    def concat(self, c1,c2): # resultado = C1+C2
+        res1 = []
+        res2= []
+        
+        for i in range (c1.tamanio()): #en el rango del tamaño de c1
+           
+            res1.append(c1.sacar())
+        
+        c1.agregar(res1)
+            
+        for i in range (c2.tamanio()):
+            
+            res2.append(c2.sacar())
+        
+        c2.agregar(res2)
+        
+        resultado = res1 + res2
+        print(c1)
+        print(c2)
+        return resultado
+    
+c1 = Cola()
+c2 = Cola()
+c3 = Cola()
+
+c1.agregar(2)
+c1.agregar(3)
+c1.agregar(4)
+
+c2.agregar(5)
+c2.agregar(6)
+c2.agregar(7)
+
+assert c3.concat(c1,c2) == [2, 3, 4, 5, 6, 7]
+
+
